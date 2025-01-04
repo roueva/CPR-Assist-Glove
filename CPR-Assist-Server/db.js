b48b6cb510a81e95856bed353cfabf20f9904de8
roueva <evarouka@gmail.com>
2025-01-04 15:17:09 +0200

    Fix database connection environment variables

--- CPR-Assist-Server/db.js
+++ CPR-Assist-Server/db.js
@@ -1,14 +1,36 @@
 const { Pool } = require('pg');
 require('dotenv').config(); // Load environment variables
 
-// Create a new pool instance
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.POSTGRES_PORT || 5432,
+console.log('Database connection details:', {
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    port: process.env.POSTGRES_PORT,
+});
 
+// Create a new pool instance
+//const pool = new Pool({     for local
+  //  user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  //  database: process.env.DB_DATABASE,
+  //  password: process.env.DB_PASSWORD,
+  //  port: process.env.POSTGRES_PORT || 5432,
+
+// });
+
+
+// Create a new pool instance with correct environment variables
+const pool = new Pool({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT || 5432,
+});
+
+pool.on('error', (err) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
 });
 
 // Export the pool to use in other files
